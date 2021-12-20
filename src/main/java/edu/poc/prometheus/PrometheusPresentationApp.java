a0d36b27c10913e795a513f9016d7ee216836d5e
Renata dos Santos Fraga <renata.fraga@picpay.com>
2021-12-20 14:44:48 -0300

    :tada: adding initial structure

---
+++ src/main/java/edu/poc/prometheus/PrometheusPresentationApp.java
@@ -0,0 +1,13 @@
+package edu.poc.prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusPresentationApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusPresentationApp.class, args);
+	}
+
+}
